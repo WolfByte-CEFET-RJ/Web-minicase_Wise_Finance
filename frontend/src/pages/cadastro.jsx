04a1bb4f0922fc2fb8be7262da41a02adea6e8d4
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2023-09-02 19:34:42 -0300

    .
    .

--- frontend/src/pages/cadastro.jsx
+++ frontend/src/pages/cadastro.jsx
@@ -13,55 +13,55 @@ const Cadastro = () => {
       <div className="rounded-[60px] bg-[#FFF] md:h-[766px] md:w-[466px] p-[43px] ">
         <form action="" className="flex justify-items-center flex-col h-[100%] w-[100%]">
           <img src="/userFoto.svg" alt="" className="ml-auto mr-auto w-[174px] h-[171px]" />
-          <h1 className="mt-[37px]">Nome Completo:</h1>
+          <h1 className="mt-[37px] font-medium">Nome Completo:</h1>
             <input
-              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px]"
+              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px] pb-[10px] pl-[10px] font-black"
               type="text"
               name="nomeCompleto"
               id="nomeCompleto"
               placeholder="Digite seu nome completo"
               onChange=""
             />
-          <h1 className="mt-[10px]">Nome de Usuário:</h1>
+          <h1 className="mt-[10px] font-medium">Nome de Usuário:</h1>
           <input
-              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px]"
+              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px] pb-[10px] pl-[10px] font-black"
               type="text"
               name="nomeUsuario"
               id="nomeUsuario"
-              placeholder="Digite seu nome de Usuário"
+              placeholder="Digite seu nome de usuário"
               onChange=""
             />
-          <h1 className="mt-[10px]">Email:</h1>
+          <h1 className="mt-[10px] font-medium">Email:</h1>
           <input
-              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px]"
+              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px] pb-[10px] pl-[10px] font-black"
               type="text"
               name="email"
               id="email"
-              placeholder="Digite seu E-mail"
+              placeholder="Digite seu e-mail"
               onChange=""
             />
-          <h1 className="mt-[10px]">Senha:</h1>
+          <h1 className="mt-[10px] font-medium">Senha:</h1>
           <input
-              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px]"
+              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px] pb-[10px] pl-[10px] font-black"
               type="text"
               name="senha"
               id="senha"
               placeholder="Digite sua senha"
               onChange=""
             />
-          <h1 className="mt-[10px]">Confirmar Senha:</h1>
+          <h1 className="mt-[10px] font-medium">Confirmar Senha:</h1>
           <input
-              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px]"
+              className="pt-3 w-[100%] h-[46px] border-2 border-green rounded-[9px] pb-[10px] pl-[10px] font-black"
               type="text"
               name="confirmarSenha"
               id="confirmarSenha"
               placeholder="Confirme sua senha"
               onChange=""
             />
-          <button className="mt-[15px] border-2 border-2 border-black rounded-[9px] bg-green text-[#FFF]">
+          <button className="mt-[15px] border-2 border-2 border-black rounded-[9px] bg-green text-[#FFF] font-black">
             Cadastre-se
           </button>
-          <button className="border-2 border-black rounded-[9px] bg-green text-[#FFF] mt-[15px]">
+          <button className="border-2 border-black rounded-[9px] bg-green text-[#FFF] mt-[15px] font-black">
             Voltar
         </button>
         </form>
