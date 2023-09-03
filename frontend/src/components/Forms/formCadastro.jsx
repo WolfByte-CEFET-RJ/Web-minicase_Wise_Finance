08c169ed340ec6da2d2750bc61ca10ba88d219d9
Daniel Vieira Medeiros <dmedeiros1209@gmail.com>
2023-09-03 16:15:33 -0300

    Espaçamento
    Mudei a forma de fazer o espaçamento dos botões para o resto do form

--- frontend/src/components/Forms/formCadastro.jsx
+++ frontend/src/components/Forms/formCadastro.jsx
@@ -1,15 +1,79 @@
-import ConjuntoInput from "../componentsCadastro/conjuntosInputs"
-
+import Input from "../componentsCadastro/inputCadastro"
+import React, { useState } from "react";
 import Button from "../componentsCadastro/buttonCadastro"
 
 const FormCadastro = () => {
+    const [name, setName] = useState("");
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [passVer, setPassVer] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
+    const handlePasswordToggle = () => {
+      setShowPassword(!showPassword);
+    };
+
+    const handleChange = (event, setText) => {
+      setText(event.target.value);
+    };
    
     return(
         <form action="" className="flex justify-items-center flex-col h-[100%] w-[100%]">
 
         <img src="/userFoto.svg" alt="" className="ml-auto mr-auto w-[174px] h-[171px]" />
-        <ConjuntoInput/>
+        <div className= "mb-[20px]">
+          <h1 className="mt-[37px] font-medium">Nome Completo:</h1>
+
+          <Input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite seu nome completo"
+          onChange={(event) => handleChange(event, setName)}
+          />
+
+          <h1 className="mt-[10px] font-medium">Nome de Usuário:</h1>
+
+          <Input
+          type="text"
+          name="userName"
+          id="userName"
+          placeholder="Digite seu nome de usuário"
+          onChange={(event) => handleChange(event, setUserName)}
+          />
+
+          <h1 className="mt-[10px] font-medium">Email:</h1>
+
+          <Input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+          onChange={(event) => handleChange(event, setEmail)}
+          />
+
+          <h1 className="mt-[10px] font-medium">Senha:</h1>
+
+          <Input
+          type="text"
+          name="pass"
+          id="pass"
+          placeholder="Digite sua senha"
+          onChange={(event) => handleChange(event, setPass)}
+          />
+
+          <h1 className="mt-[10px] font-medium">Confirmar Senha:</h1>
+
+          <Input
+          className = "mb-[100px]"
+          type="text"
+          name="passVer"
+          id="passVer"
+          placeholder="Confirme sua senha"
+          onChange={(event) => handleChange(event, setPassVer)}
+          />
+        </div>
         <Button Text = "Cadastre-se"/>
         <Button Text="Voltar"/>
       </form>
