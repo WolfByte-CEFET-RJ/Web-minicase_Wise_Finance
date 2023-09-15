74fe5c569cc73f16e833c0ef0167e19c6320a248
laraFRamos <lara.fernandes.ramos2003@gmail.com>
2023-09-15 19:56:04 -0300

    Arquivo p/ gerar chave retirado

--- backend/src/utils/gerarChaveJWT.js
+++
@@ -1,3 +0,0 @@
-const crypto = require('crypto');
-const jwtKey = crypto.randomBytes(32).toString('hex'); // Gera uma chave JWT de 256 bits (32 bytes) em formato hexadecimal
-console.log("Chave JWT gerada: ", jwtKey);
