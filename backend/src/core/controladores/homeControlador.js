ce82094e104269d7ea5dd4baa69acfe500b1d0b1
laraFRamos <lara.fernandes.ramos2003@gmail.com>
2023-09-15 21:11:51 -0300

    protótipo home

---
+++ backend/src/core/controladores/homeControlador.js
@@ -0,0 +1,84 @@
+const DespesaFixaService = require('../services/despesaFixaService');
+
+async function createDespesaFixa(req, res) {
+  const userId = req.user.id;
+  const despesaFixaData = req.body;
+
+  try {
+    const result = await DespesaFixaService.createDespesaFixa(userId, despesaFixaData);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function getAllDespesasFixas(req, res) {
+  const userId = req.user.id;
+
+  try {
+    const despesasFixas = await DespesaFixaService.getAllDespesasFixas(userId);
+    res.json(despesasFixas);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function getDespesaFixaById(req, res) {
+  const userId = req.user.id;
+  const despesaFixaId = req.params.id;
+
+  try {
+    const despesaFixa = await DespesaFixaService.getDespesaFixaById(userId, despesaFixaId);
+
+    if (!despesaFixa) {
+      return res.status(404).json({ error: 'Despesa fixa não encontrada' });
+    }
+
+    res.json(despesaFixa);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function updateDespesaFixa(req, res) {
+  const userId = req.user.id;
+  const despesaFixaId = req.params.id;
+  const despesaFixaData = req.body;
+
+  try {
+    const result = await DespesaFixaService.updateDespesaFixa(userId, despesaFixaId, despesaFixaData);
+
+    if (result === 0) {
+      return res.status(404).json({ error: 'Despesa fixa não encontrada' });
+    }
+
+    res.json({ message: 'Despesa fixa atualizada com sucesso' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function deleteDespesaFixa(req, res) {
+  const userId = req.user.id;
+  const despesaFixaId = req.params.id;
+
+  try {
+    const result = await DespesaFixaService.deleteDespesaFixa(userId, despesaFixaId);
+
+    if (result === 0) {
+      return res.status(404).json({ error: 'Despesa fixa não encontrada' });
+    }
+
+    res.json({ message: 'Despesa fixa excluída com sucesso' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = {
+  createDespesaFixa,
+  getAllDespesasFixas,
+  getDespesaFixaById,
+  updateDespesaFixa,
+  deleteDespesaFixa,
+};
---
+++ backend/src/core/servicos/homeServico.js
@@ -0,0 +1,60 @@
+// services/despesaFixaService.js
+
+const database = require('../../database/index');
+
+async function createDespesaFixa(userId, despesaFixaData) {
+  try {
+    return await knex('Despesa_Fixa').insert({
+      ID_Usuario: userId,
+      ...despesaFixaData,
+    });
+  } catch (error) {
+    throw new Error('Erro ao criar a despesa fixa');
+  }
+}
+
+async function getAllDespesasFixas(userId) {
+  try {
+    return await knex('Despesa_Fixa').where('ID_Usuario', userId);
+  } catch (error) {
+    throw new Error('Erro ao buscar despesas fixas');
+  }
+}
+
+async function getDespesaFixaById(userId, despesaFixaId) {
+  try {
+    return await knex('Despesa_Fixa')
+      .where({ ID: despesaFixaId, ID_Usuario: userId })
+      .first();
+  } catch (error) {
+    throw new Error('Erro ao buscar a despesa fixa');
+  }
+}
+
+async function updateDespesaFixa(userId, despesaFixaId, despesaFixaData) {
+  try {
+    return await knex('Despesa_Fixa')
+      .where({ ID: despesaFixaId, ID_Usuario: userId })
+      .update(despesaFixaData);
+  } catch (error) {
+    throw new Error('Erro ao atualizar a despesa fixa');
+  }
+}
+
+async function deleteDespesaFixa(userId, despesaFixaId) {
+  try {
+    return await knex('Despesa_Fixa')
+      .where({ ID: despesaFixaId, ID_Usuario: userId })
+      .delete();
+  } catch (error) {
+    throw new Error('Erro ao excluir a despesa fixa');
+  }
+}
+
+module.exports = {
+  createDespesaFixa,
+  getAllDespesasFixas,
+  getDespesaFixaById,
+  updateDespesaFixa,
+  deleteDespesaFixa,
+};
