650985968f5002c61cb7741e734d9fd0843db070
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2023-09-17 13:14:30 -0300

    .
    .

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
 <div className="  w-screen" >
       <div className = " fixed w-[45%] mt-[-12%] h-screen bg-no-repeat bg-cover flex items-center	justify-center flex-col" style = {{backgroundImage:"url(/Fundo.svg)"}}>
-          <img className="mt-[-5%] " src= "/android-chrome-192x192.png" />
+          <img className="mt-[-5%]" alt="ramoLogo" src= "/android-chrome-192x192.png" />
           <h1 className="mt-[10%] font-black text-[white] text-[48px]">Wise Finance</h1>
       </div>
       <div className=" mt-[12%] ml-[60%] w-[30%]">
