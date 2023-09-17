8fc457869ef583e31446c8a410e9cde179073e3f
Daniel Vieira Medeiros <dmedeiros1209@gmail.com>
2023-09-17 12:59:11 -0300

    ....

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -3,12 +3,12 @@ import Form from "../components/Forms/formLogin"
 
 const Login = () => {
   return (
-<div className="border-4 border-[black] h-screen w-screen" >
-      <div className = " fixed w-[50%] h-screen bg-no-repeat bg-cover flex items-center	justify-center flex-col" style = {{backgroundImage:"url(/Fundo.svg)"}}>
+<div className="  w-screen" >
+      <div className = " fixed w-[45%] mt-[-12%] h-screen bg-no-repeat bg-cover flex items-center	justify-center flex-col" style = {{backgroundImage:"url(/Fundo.svg)"}}>
           <img className="mt-[-5%] " src= "/android-chrome-192x192.png" />
           <h1 className="mt-[10%] font-black text-[white] text-[48px]">Wise Finance</h1>
       </div>
-      <div className=" mt-[10%] ml-[60%] w-[30%]">
+      <div className=" mt-[12%] ml-[60%] w-[30%]">
         <Form/>
       </div>
 </div>
