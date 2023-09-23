df3b1b3f6422c5743008fbf3b6ff2d4c44408430
Gustavo  Barros <gustavoandrade0125@gmail.com>
2023-09-23 01:18:07 -0300

    crud receita variavel

--- backend/src/core/servicos/receitaServico.js
+++ backend/src/core/servicos/receitaServico.js
@@ -77,7 +77,7 @@ async function getAllDespesasFixasServico() {
         message: error.message,
       };
     }
-  }
+}
 
   async function updateDespesaFixaServico(userId, despesaId, nome, valor, descricao, dataPagamento) {
     try {
@@ -125,7 +125,7 @@ async function getAllDespesasFixasServico() {
         message: error.message,
       };
     }
-  }
+}
   
 
 async function deleteDespesaFixaServico(userId, despesaId) {
@@ -159,8 +159,162 @@ async function deleteDespesaFixaServico(userId, despesaId) {
   }
 }
 
-
 //RECEITAS VARIÁVEIS
+
+async function createReceitaVarServico (userId, nome, valor, descricao, dataPagamento) {
+  try {
+    
+    if (
+      !nome ||
+      !valor ||
+      !descricao ||
+      !dataPagamento
+    ) {
+      throw new Error("Preencha todos os campos obrigatórios.");
+    }
+
+    const usuario = await database("Usuario").select('*').where("id",userId).first()
+    if(!usuario){
+      throw new Error("Usuário não encontrado");
+    }
+
+    const novaReceitaVar = { 
+      ID_Usuario : userId,
+      Nome: nome,
+      Valor: valor,
+      Data: dataPagamento, 
+      Descricao: descricao,
+    }
+
+    await database("Receita_Variavel").insert(novaReceitaVar);
+    return {
+      status: true,
+      message: `Receita adicionada ao usuário ${userId}`,
+    };
+  
+  } catch (error) {
+    return {
+      status: false,
+      message: error.message,
+    };
+  }
+}
+
+async function getAllReceitaVar_Usuario_Servico(userId) {
+  try {
+    console.log(userId)
+    return await database('Receita_Variavel').select('*').where('ID_Usuario', userId);
+  } catch (error) {
+    return {
+      status: false,
+      message: error.message,
+    };
+  }
+}
+
+async function getReceitaVarByIdServico(userId, receitaVarId) {
+  try {
+    return await database('Receita_Variavel')
+      .select("*")
+      .where({ ID: receitaVarId, ID_Usuario: userId })
+      .first();
+  } catch (error) {
+    return {
+      status: false,
+      message: error.message,
+    };
+  }
+}
+
+async function getAllReceitaVarServico() {
+    try {
+      return await database('Receita_Variavel').select('*');
+    } catch (error) {
+      return {
+        status: false,
+        message: error.message,
+      };
+    }
+}
+
+  async function updateReceitaVarServico(userId, receitaId, nome, valor, descricao, dataPagamento) {
+    try {
+      
+      if (
+        !nome ||
+        !valor ||
+        !descricao ||
+        !dataPagamento
+      ) {
+        throw new Error("Preencha todos os campos obrigatórios.");
+      }
+  
+      const usuario = await database("Usuario").select('*').where("id",userId).first()
+      if(!usuario){
+        throw new Error("Usuário não encontrado");
+      }
+  
+      const despesa = await database("Receita_Variavel").select('*').where("id",receitaId).first()
+      if(!despesa){
+        throw new Error("Receita não encontrada");
+      }
+  
+      if(usuario.ID !== despesa.ID_Usuario){
+        throw new Error("Receita não relacionada ao usuário");
+      }
+      
+      const novaReceitaVar = { 
+        ID_Usuario : userId,
+        Nome: nome,
+        Valor: valor,
+        Data: dataPagamento, 
+        Descricao: descricao,
+      }
+  
+      await database("Receita_Variavel").update(novaReceitaVar).where("id",receitaId);
+      return {
+        status: true,
+        message: `Informações da despesa ${receitaId} atualizadas com sucesso!`,
+      };
+    
+    } catch (error) {
+      return {
+        status: false,
+        message: error.message,
+      };
+    }
+}
+  
+async function deleteReceitaVarServico(userId, receitaId) {
+  try {
+
+    const usuario = await database("Usuario").select('*').where("id",userId).first()
+    if(!usuario){
+      throw new Error("Usuário não encontrado");
+    }
+
+    const despesa = await database("Receita_Variavel").select('*').where("id",receitaId).first()
+    if(!despesa){
+      throw new Error("Receita não encontrada");
+    }
+
+    if(usuario.ID!==despesa.ID_Usuario){
+      throw new Error("Receita não relacionada ao usuário");
+    }
+    
+    await database("Receita_Variavel").where("id",receitaId).delete();
+    return {
+      status: true,
+      message: `Receita deleteda com sucesso!`,
+    };
+  
+  } catch (error) {
+    return {
+      status: false,
+      message: error.message,
+    };
+  }
+}
 
 module.exports = {
   //RECEITAS FIXAS
@@ -171,12 +325,12 @@ module.exports = {
   getAllReceitaFixaServico,
   getAllReceitaFixa_Usuario_Servico,
   getReceitaFixaByIdServico,
+  */
   //RECEITAS VARIÁVEIS
-  /*
   createReceitaVarServico,
   updateReceitaVarServico,
   deleteReceitaVarServico,
   getAllReceitaVarServico,
   getAllReceitaVar_Usuario_Servico,
-  getReceitaVarByIdServico,*/
+  getReceitaVarByIdServico,
 };
