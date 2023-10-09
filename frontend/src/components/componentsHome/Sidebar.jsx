92b7e8a0e3df3f442745a2830932c92e65992e62
Daniel Vieira Medeiros <dmedeiros1209@gmail.com>
2023-10-08 22:21:16 -0300

    ...

--- frontend/src/components/componentsHome/Sidebar.jsx
+++ frontend/src/components/componentsHome/Sidebar.jsx
@@ -18,10 +18,40 @@ function Sidebar (){
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
-            <div className="flex items-center">
-                <img className= "mt-[-5%] w-[33px] h-[32px] " alt="ramoLogo" src= "/android-chrome-192x192.png"/>
-                <h1 className='text-[12px]' > Wise Finance </h1>
+          <div className= "Container-LogoNome bg-[#000]  ">
+            <div className="flex items-center justify-center fixed " >
+                <img className= "mt-[-5%] w-[33px] h-[32px] mr-[30%] ml-[30%]  " alt="ramoLogo" src= "/android-chrome-192x192.png"/>
             </div>
+            <div className='text-[12px] ml-[85px] mt-[6px] fixed ' > Wise Finance </div>
+          </div>
+
+            <button className= "Container-DashBoard bg-[#000]">
+              <div className="flex items-center justify-center fixed  mt-[3%] ml-[0.35%]  " >
+                  <img className= "mt-[-5%] w-[20px] h-[20px] mr-[30%] ml-[30%]  " alt="ramoLogo" src= "/DashBoard.png"/>
+              </div>
+              <div className='text-[12px] ml-[87px] mt-[6px] fixed  mt-[3%]  ' > Dashboard </div>
+            </button>
+
+            <button className= "Container-Perfil bg-[#000]">
+              <div className="flex items-center justify-center fixed  mt-[6%] ml-[0.35%]  " >
+                  <img className= "mt-[-5%] w-[20px] h-[20px] mr-[30%] ml-[30%]  " alt="ramoLogo" src= "/Perfil.png"/>
+              </div>
+              <div className='text-[12px] ml-[87px] mt-[6px] fixed  mt-[6%]  ' > Perfil </div>
+            </button>
+
+            <button className= "Container-Baixar bg-[#000]">
+              <div className="flex items-center justify-center fixed  mt-[9%] ml-[0.35%]  " >
+                  <img className= "mt-[-5%] w-[20px] h-[20px] mr-[30%] ml-[30%]  " alt="ramoLogo" src= "/Baixar.png"/>
+              </div>
+              <div className='text-[12px] ml-[87px] mt-[6px] fixed  mt-[9%]  ' > Baixar Relatório </div>
+            </button>
+
+            <button className= "Container-Logout bg-[#000] ">
+              <div className="flex items-center justify-center fixed  mt-[45%] ml-[0.35%]  " >
+                  <img className= "mt-[-5%] w-[20px] h-[20px] mr-[30%] ml-[30%]  " alt="ramoLogo" src= "/logout.png"/>
+              </div>
+              <div className='text-[12px] ml-[87px] mt-[6px] fixed  mt-[45%]  ' > Logout </div>
+            </button>
         </div>
       );
 }
