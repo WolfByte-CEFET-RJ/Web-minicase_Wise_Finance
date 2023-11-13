d2135e7e211b39356f4f17a9eb18cf04fca2bc02
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2023-11-13 01:10:20 -0300

    .

--- frontend/src/components/auth.jsx
+++ frontend/src/components/auth.jsx
@@ -7,27 +7,30 @@ export const AuthContext = createContext({});
 export default function AuthProvider({ children }) {
   const [userID, setUserID] = useState(null);
   const [token, setToken] = useState(null);
-  const isAuthenticated = !!userID;
-
+  const [authenticated, setAuthenticated] = useState(false);
+  const pageLogin = "http://localhost:3000/";
+  const pageCadastro = "http://localhost:3000/cadastro";
+  
   useEffect(() => {
     const accessToken = Cookies.get("access_token");
-
+    const url = window.location.href;
+    
     if (accessToken) {
       try {
         const decodedToken = jwtDecode(accessToken);
         setUserID(decodedToken.id);
         setToken(accessToken);
+        setAuthenticated(true);
       } catch (error) {
-        console.error("Error decoding token:", error);
+        console.error("Erro desemcriptando token", error);
       }
-    } else {
-      console.log("No access token found. Redirecting...");
+    } else if (authenticated && (url !== pageLogin && url !== pageCadastro)) {
+      window.location.href = "http://localhost:3000/";
     }
-  }, []);
-  console.log(userID);
-  console.log(token);
+  }, [authenticated]);
+
   return (
-    <AuthContext.Provider value={{ userID, isAuthenticated, token }}>
+    <AuthContext.Provider value={{ userID, token }}>
       {children}
     </AuthContext.Provider>
   );
