32d843573c08acfaf8ab0c46b7dba7e2abf9d5f5
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2023-12-09 16:43:00 -0300

    .
    .

--- frontend/src/components/modais/modaisDespesas/fixas/modalEditarDespesasFixas.jsx
+++ frontend/src/components/modais/modaisDespesas/fixas/modalEditarDespesasFixas.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import useApi from "../../../../hooks/useApi";
 import { AuthContext } from "../../../auth";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const ModalDespesasFixas = ({ Aberto, Fechado }) => {
+const ModalDespesasFixas = ({ idDespesa, Aberto, Fechado }) => {
   const [nome, setNome] = useState("");
   const [descricao, setDescricao] = useState("");
   const [preco, setPreco] = useState("");
   const { userID, token } = useContext(AuthContext);
+  const [userData, setUserData] = useState([]);
   const api = useApi();
   const dataAtual = new Date().toISOString();
 
@@ -25,24 +26,49 @@ const ModalDespesasFixas = ({ Aberto, Fechado }) => {
     return `${ano}-${mes}-${dia}`;
   }
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userData = await api.get(`http://localhost:5000/despesa_fixa/${idDespesa}`, {
+          body: {
+            userId: userID,
+            despesaFixalId: idDespesa,
+          },
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUserData(userData.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (Aberto) {
+      fetchData();
+    }
+  }, [Aberto, idDespesa, api, userID, token]);
+
   async function handleEnvio(event) {
     event.preventDefault();
     const body = {
       userId:  userID,
+      despesaId: idDespesa,
       nome: nome,
       valor: preco,
       descricao: descricao,
       dataPagamento: formatarDataParaEnvio(dataAtual),
     };
     try {
-      const response = await api.post("http://localhost:5000/despesa_fixa", body, {
+      const response = await api.patch(`http://localhost:5000/despesa_fixa/${idDespesa}`, body, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
       console.log(response.data);
       if (response.data.status === true) {
-        toast.success("Despesa adicionada com sucesso!");
+        toast.success("Despesa alterada com sucesso!");
+        window.location.reload();
       } else if (response.data.status === false){
         toast.error(response.data.message.toString())
       }
@@ -79,7 +105,7 @@ const ModalDespesasFixas = ({ Aberto, Fechado }) => {
                 <input
                   id="nome"
                   className="border border-black rounded-[5px] w-[380px] h-[40px] mb-[30px] pl-[5px]"
-                  placeholder="Digite o nome"
+                  placeholder={userData.Nome}
                   onChange={(event) => handleChange(event, setNome)}
                 />
               </div>
@@ -88,7 +114,7 @@ const ModalDespesasFixas = ({ Aberto, Fechado }) => {
                 <textarea
                   id="detalhes"
                   className="border border-black rounded-[5px] w-[380px] h-[150px] mb-[30px] pl-[5px] pt-[5px]"
-                  placeholder="Detalhes da despesa"
+                  placeholder={userData.Descricao}
                   onChange={(event) => handleChange(event, setDescricao)}
                 />
               </div>
@@ -98,7 +124,7 @@ const ModalDespesasFixas = ({ Aberto, Fechado }) => {
                 <input
                   id="valor"
                   className="border border-black rounded-[5px] w-[380px] mb-[30px] h-[40px] pl-[5px]"
-                  placeholder="Digite o valor"
+                  placeholder={userData.Valor}
                   onChange={(event) => handleChange(event, setPreco)}
                 />
               </div>
--- frontend/src/components/modais/modaisDespesas/variaveis/modalEditarDespesasVariaveis.jsx
+++ frontend/src/components/modais/modaisDespesas/variaveis/modalEditarDespesasVariaveis.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import useApi from "../../../../hooks/useApi";
 import { AuthContext } from "../../../auth";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const ModalDespesasVariaveis = ({ Aberto, Fechado }) => {
+const ModalDespesasVariaveis = ({ idDespesa, Aberto, Fechado }) => {
   const [nome, setNome] = useState("");
   const [descricao, setDescricao] = useState("");
   const [preco, setPreco] = useState("");
   const { userID, token } = useContext(AuthContext);
+  const [userData, setUserData] = useState([]);
   const api = useApi();
   const dataAtual = new Date().toISOString();
 
@@ -25,24 +26,49 @@ const ModalDespesasVariaveis = ({ Aberto, Fechado }) => {
     return `${ano}-${mes}-${dia}`;
   }
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userData = await api.get(`http://localhost:5000/despesa_var/${idDespesa}`, {
+          body: {
+            userId: userID,
+            despesaVariavelId: idDespesa,
+          },
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUserData(userData.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (Aberto) {
+      fetchData();
+    }
+  }, [Aberto, idDespesa, api, userID, token]);
+
   async function handleEnvio(event) {
     event.preventDefault();
     const body = {
       userId:  userID,
+      despesaId: idDespesa,
       nome: nome,
       valor: preco,
       descricao: descricao,
       dataPagamento: formatarDataParaEnvio(dataAtual),
     };
     try {
-      const response = await api.post("http://localhost:5000/despesa_var", body, {
+      const response = await api.patch(`http://localhost:5000/despesa_var/${idDespesa}`, body, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
       console.log(response.data);
       if (response.data.status === true) {
-        toast.success("Despesa adicionada com sucesso!");
+        toast.success("Despesa alterada com sucesso!");
+        window.location.reload();
       } else if (response.data.status === false){
         toast.error(response.data.message.toString())
       }
@@ -51,6 +77,7 @@ const ModalDespesasVariaveis = ({ Aberto, Fechado }) => {
       console.log(error);
     }
   }
+
   if (!Aberto) return null;
   return (
     <div className="w-[100.2%] h-[100.1%]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[rgba(0,0,0,0.7)]  rounded-[96px]">
@@ -62,55 +89,55 @@ const ModalDespesasVariaveis = ({ Aberto, Fechado }) => {
             src="X.png"
             onClick={Fechado}
           />
-          <h1 className=" mt-[-3.6%] ml-[25%] text-[50px] font-black text-green">
-            Despesas Variaveis
+          <h1 className=" mt-[-3.6%] ml-[30%] text-[50px] font-black text-green">
+          Despesas Variaveis
           </h1>
-          <h1 className=" ml-[30%] text-[10px] font-black text-green">
-            Lembrando que essa é uma despesa variável e será apagada após um mês
+          <h1 className=" ml-[33%] text-[10px] font-black text-green">
+          Lembrando que essa é uma despesa variável e será apagada após um mês
           </h1>
           <div>
             <form 
               className="flex flex-col items-center text-[15px]"
               onSubmit={handleEnvio}
-              >
+            >
               <div className="mb-30 mt-[20px]">
-                <h1 className="text-black">Nome Receita:</h1>
+                <h1 className="text-black">Nome Despesa:</h1>
                 <input
                   id="nome"
                   className="border border-black rounded-[5px] w-[380px] h-[40px] mb-[30px] pl-[5px]"
-                  placeholder="Digite o nome"
+                  placeholder={userData.Nome}
                   onChange={(event) => handleChange(event, setNome)}
                 />
               </div>
               <div className="mb-30">
-                <h1 className="text-black">Descrição Receita:</h1>
+                <h1 className="text-black">Descrição Despesa:</h1>
                 <textarea
                   id="detalhes"
                   className="border border-black rounded-[5px] w-[380px] h-[150px] mb-[30px] pl-[5px] pt-[5px]"
-                  placeholder="Detalhes da despesa"
+                  placeholder={userData.Descricao}
                   onChange={(event) => handleChange(event, setDescricao)}
                 />
               </div>
               <div className="mb-30 ml-[-2%]">
-                <h1 className="text-black ml-[5%]">Valor Receita:</h1>
+                <h1 className="text-black ml-[5%]">Valor Despesa:</h1>
                 <label className="text-black">R$</label>
                 <input
                   id="valor"
                   className="border border-black rounded-[5px] w-[380px] mb-[30px] h-[40px] pl-[5px]"
-                  placeholder="Digite o valor"
+                  placeholder={userData.Valor}
                   onChange={(event) => handleChange(event, setPreco)}
                 />
               </div>
               <button 
                 className="border border-black rounded-[9px] bg-[#1E7B71] mb-[10px] text-white h-[31px] w-[380px]"
                 type="submit"
-                >
+              >
                 Salvar
               </button>
               <button 
                 className="border border-black rounded-[9px] bg-[#1E7B71] mb-[10px] text-white h-[31px] w-[380px]"
                 onClick={Fechado}
-                >
+              >
                 Cancelar
               </button>
             </form>
