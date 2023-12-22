7bffdcc507fb43b9a4c891e789770e6f2634fe33
Daniel Vieira Medeiros <dmedeiros1209@gmail.com>
2023-12-21 22:27:31 -0300

    mexendo perfil

--- frontend/src/pages/perfil.jsx
+++ frontend/src/pages/perfil.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect  } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useApi from "../hooks/useApi";
@@ -6,21 +6,17 @@ import Sidebar from "../components/sideBar";
 import { AuthContext } from "../components/auth";
 
 const Perfil = () => {
+  const api = useApi();
   const { userID, token } = useContext(AuthContext);
-  const [name, setName] = useState("Lucas Teixeira dos Santos");
-  const [email, setEmail] = useState("lucast.santos2003@gmail.com");
-  const [userName, setUserName] = useState("luquinhas");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
   const [pass, setPass] = useState("123");
   const [passVer, setPassVer] = useState("123");
   const [editarNome, setEditarNome] = useState(false);
   const [editarPassword, setEditarPassword] = useState(false);
   const [editarPasswordVer, setEditarPasswordVer] = useState(false);
-  // const handlePasswordToggle = () => {
-  //   setShowPassword(!showPassword);
-  // };
-  // const handlePasswordVerToggle = () => {
-  //   setShowPasswordVer(!showPasswordVer);
-  // };
+
   const handleEditartrue = () => {
     setEditarNome(true);
     setEditarPassword(true);
@@ -34,6 +30,36 @@ const Perfil = () => {
   const handleChange = (event, setText) => {
     setText(event.target.value);
   };
+
+  useEffect(() => {
+    const fetchUsuario = async () => {
+      try {
+        const response = await api.get("http://localhost:5000/usuario", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        console.log("Dados Usuario:", response.data);
+        setName(response.data.usuario.Nome);
+        setEmail(response.data.usuario.Email);
+        setUserName(response.data.usuario.Username);       
+      } catch (error) {
+        console.error("Erro na solicitação:", error);
+      }
+    };
+   
+    fetchUsuario();
+  }, [api, token]);
+
+  async function handleEnvio(event) {
+    event.preventDefault();
+
+    const response = await api.patch("http://localhost:5000/usuario");
+  };
+
+
+
   return (
     <div
       className="h-screen bg-no-repeat bg-cover relative "
@@ -54,10 +80,38 @@ const Perfil = () => {
               {email}
             </h1>
           </div>
+
           <form
             action=""
             className="flex flex-col h-[100%] w-[50%] mt-[12%] mb-[12%]"
+            onSubmit={handleEnvio}
           >
+            <div className="mb-30  ">
+              <h1 className="font-black text-[12px]">Nome completo:</h1>
+              <div className="border-2 border-green rounded-[5px] w-[380px] h-[51px] mb-[30px] pl-[5px] flex align-itens">
+                {editarPassword === true ? (
+                  <input
+                    id="nome"
+                    className="w-[89%] h-[100%] ml-[-5px] pl-[5px]"
+                    placeholder={name}
+                    onChange={(event) => handleChange(event, setName)}
+                  />
+                ) : (
+                  <h1 className="w-[89%] h-[100%] ml-[-5px] pl-[5px]  pt-[3%]">
+                    {" "}
+                    {name}{" "}
+                  </h1>
+                )}
+                <div className="border-l border-green w-[10%] h-[34px] mt-[1.5%]">
+                  <img
+                    src="/Lapis.png"
+                    alt=""
+                    className=" w-[18px] h-[19px] ml-[10px] mt-[6px]"
+                    onClick={handleEditartrue}
+                  />
+                </div>
+              </div>
+            </div>
             <div className="mb-30  ">
               <h1 className="font-black text-[12px]">Nome de Usuário:</h1>
               <div className="border-2 border-green rounded-[5px] w-[380px] h-[51px] mb-[30px] pl-[5px] flex align-itens">
@@ -66,6 +120,7 @@ const Perfil = () => {
                     id="nome"
                     className="w-[89%] h-[100%] ml-[-5px] pl-[5px]"
                     placeholder={userName}
+                    onChange={(event) => handleChange(event, setUserName)}
                   />
                 ) : (
                   <h1 className="w-[89%] h-[100%] ml-[-5px] pl-[5px]  pt-[3%]">
@@ -91,6 +146,7 @@ const Perfil = () => {
                     id="senha"
                     className="w-[89%] h-[100%] ml-[-5px] pl-[5px]"
                     placeholder={pass}
+                    onChange={(event) => handleChange(event, setPass)}
                   />
                 ) : (
                   <h1 className="w-[89%] h-[100%] ml-[-5px] pl-[5px]  pt-[3%]">
@@ -117,13 +173,17 @@ const Perfil = () => {
                   id="nome"
                   className="border-2 border-green rounded-[5px] w-[380px] h-[51px] mb-[30px] pl-[5px]"
                   placeholder={passVer}
+                  onChange={(event) => handleChange(event, setPassVer)}
                 />
-                <button className="border-2 border-black rounded-[9px] bg-[#1E7B71] mb-[10px] font-black text-white h-[31px] w-[380px]">
+                <button className="border-2 border-black rounded-[9px] bg-[#1E7B71] mb-[10px] font-black text-white h-[31px] w-[380px]"
+                type="submit"
+                >
                   Salvar
                 </button>
                 <button
                   className="border-2 border-black rounded-[9px] bg-[#1E7B71] mb-[10px] font-black text-white h-[31px] w-[380px]"
                   onClick={handleEditarFalse}
+
                 >
                   Cancelar
                 </button>
@@ -136,7 +196,7 @@ const Perfil = () => {
           </form>
         </div>
       </div>
-      <Sidebar />
+      <Sidebar />d
     </div>
   );
 };
