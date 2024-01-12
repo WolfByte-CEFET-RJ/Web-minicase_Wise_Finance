d9a8c8fb491f504d2414108a57d6c53ed395a19f
Daniel Vieira Medeiros <dmedeiros1209@gmail.com>
2024-01-12 18:36:25 -0300

    Fix: Mudanças tela home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,6 +26,7 @@ const Home = () => {
 
   const [estadoModalReceitas, setEstadoModalReceitas] = useState(false);
   const [estadoModalDespesas, setEstadoModalDespesas] = useState(false);
+
   const AbrirModalReceitas = () => {
     setEstadoModalReceitas(true);
   };
@@ -33,6 +34,7 @@ const Home = () => {
   const FecharModalReceitas = () => {
     setEstadoModalReceitas(false);
   };
+
   const AbrirModalDespesas = () => {
     setEstadoModalDespesas(true);
   };
@@ -40,6 +42,7 @@ const Home = () => {
   const FecharModalDespesas = () => {
     setEstadoModalDespesas(false);
   };
+
   const handleSaldoGeralToggle = () => {
     setShowSaldoGeral(!showSaldoGeral);
   };
@@ -92,22 +95,21 @@ const Home = () => {
   }, [api, token]);
 
   return (
-    <div
-      className="h-screen bg-no-repeat bg-cover relative"
-      style={{ backgroundImage: "url(/Fundo.svg)" }}
-    >
+    <div className="min-h-screen bg-no-repeat bg-cover relative" style={{ backgroundImage: "url(/Fundo.svg)" }}>
+      <Sidebar />
       {estadoModalReceitas || estadoModalDespesas ? (
         <div className="fixed w-full h-full bg-[rgba(0,0,0,0.7)]  left-0 top-0" />
       ) : (
         <div />
       )}
-      <div className=" w-[70%] h-[70%] ml-[1%] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white  rounded-[96px]">
+      
+      <div className="w-full md:w-[70%] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-[24px] p-4 md:p-8">
         <div className="Partedecima">
           <div className="Container-saldoGeral mt-[2%]">
-            <h1 className="text-[90px] ml-[30%]  font-black text-green">
+            <h1 className="text-[20px] md:text-[90px] ml-[5%] md:ml-[30%] font-black text-green">
               Saldo Geral
             </h1>
-            <div className="ml-[38%]  text-[49px] flex items-center font-black ">
+            <div className="ml-[5%] md:ml-[38%]  text-[16px] md:text-[49px] flex items-center font-black ">
               {showSaldoGeral ? (
                 <div>
                   <p>{saldoGeral}</p>
@@ -115,35 +117,35 @@ const Home = () => {
                     size={30}
                     weight="duotone"
                     onClick={handleSaldoGeralToggle}
-                    className="ml-[110%] mt-[-18%] cursor-pointer"
+                    className="ml-[5%] md:ml-[110%] mt-[-18%] cursor-pointer"
                   />
                 </div>
               ) : (
                 <div>
-                  <p className="ml-[20%]">******</p>
+                  <p className="ml-[5%] md:ml-[20%]">******</p>
                   <Eye
                     size={30}
                     weight="duotone"
                     onClick={handleSaldoGeralToggle}
-                    className="ml-[173%] mt-[-28%] cursor-pointer"
+                    className="ml-[5%] md:ml-[173%] mt-[-28%] cursor-pointer"
                   />
                 </div>
               )}
             </div>
           </div>
           <div className="Container-BalancoMensal">
-            <h1 className="text-[21px] ml-[37%] mt-[3%] font-black text-green">
+            <h1 className="text-[16px] md:text-[21px] ml-[5%] md:ml-[37%] mt-[3%] font-black text-green">
               Balanço Mensal:
               <span className="text-black"> R$ {balancoMensal}</span>
             </h1>
           </div>
         </div>
-        <div className="border-green border-t-2 w-[50%] ml-[25%] mt-[2%]  ">
-          <div className="mt-[5%] flex items-center justify-center">
-            <div className=" border-green border-r-2 pr-[30%]">
-              <h1 className=" text-[50px] font-black text-green">Despesas</h1>
+        <div className="border-green border-t-2 w-[50%] ml-[5%] md:ml-[25%] mt-[2%] md:mt-[2%]  ">
+          <div className="mt-[5%] md:flex items-center justify-center">
+            <div className="border-green border-r-2 md:pr-[30%]">
+              <h1 className="text-[20px] md:text-[50px] font-black text-green">Despesas</h1>
               <button
-                className=" ml-[20%] w-[120px] h-[20px] rounded-[96px] bg-[#EF0606] text-[10px] font-black text-white border border-black "
+                className="ml-[5%] md:ml-[20%] w-[120px] md:w-[120px] h-[20px] md:h-[20px] rounded-[96px] bg-[#EF0606] text-[10px] md:text-[10px] font-black text-white border border-black "
                 onClick={AbrirModalDespesas}
               >
                 Ver despesas
@@ -152,27 +154,27 @@ const Home = () => {
                 Aberto={estadoModalDespesas}
                 Fechado={FecharModalDespesas}
               />
-              <div className="text-[#EF0606] font-black text-[35px] mt-[5%]">
+              <div className="text-[#EF0606] font-black text-[20px] md:text-[35px] mt-[5%]">
                 R$ {totalDespesas}
               </div>
-              <div className="text-[10px] ml-[15%] font-black text-green">
+              <div className="text-[10px] md:ml-[5%] font-black text-green">
                 Limite de Gastos:
                 <span className="text-black"> R$ {limiteGastos}</span>
               </div>
-              <div className="text-[15px] mt-[5%] font-black text-green">
+              <div className="text-[15px] mt-[5%] md:mt-[5%] font-black text-green">
                 Despesas Fixas:
                 <span className="text-[#EF0606]"> R$ {despesasFixas}</span>
               </div>
-              <div className="text-[15px] mt-[5%] font-black text-green">
+              <div className="text-[15px] mt-[5%] md:mt-[5%] font-black text-green">
                 Despesas Variáveis:
                 <span className="text-[#EF0606]"> R$ {despesasVariaveis}</span>
               </div>
             </div>
-            <div className="ml-[30%]">
-              <h1 className=" text-[50px] font-black text-green">Receitas</h1>
+            <div className="ml-[5%] md:ml-[30%]">
+              <h1 className="text-[20px] md:text-[50px] font-black text-green">Receitas</h1>
               <div>
                 <button
-                  className="ml-[20%] w-[120px] h-[20px] rounded-[96px] bg-[#156808] text-[10px] font-black text-white border border-black "
+                  className="ml-[5%] md:ml-[20%] w-[120px] md:w-[120px] h-[20px] md:h-[20px] rounded-[96px] bg-[#156808] text-[10px] md:text-[10px] font-black text-white border border-black "
                   onClick={AbrirModalReceitas}
                 >
                   {" "}
@@ -183,14 +185,14 @@ const Home = () => {
                   Fechado={FecharModalReceitas}
                 />
               </div>
-              <div className="text-[#156808] font-black text-[35px] mt-[5%]">
+              <div className="text-[#156808] font-black text-[20px] md:text-[35px] mt-[5%]">
                 R$ {totalReceitas}
               </div>
-              <div className="text-[15px] mt-[5%] font-black text-green">
+              <div className="text-[15px] mt-[5%] md:mt-[5%] font-black text-green">
                 Receitas Fixas:
                 <span className="text-[#156808] "> R$ {receitasFixas}</span>
               </div>
-              <div className="text-[15px] mt-[5%] font-black text-green">
+              <div className="text-[15px] mt-[5%] md:mt-[5%] font-black text-green">
                 Receitas Variáveis:
                 <span className="text-[#156808] "> R$ {receitasVariaveis}</span>
               </div>
@@ -198,7 +200,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <Sidebar />
+      
     </div>
   );
 };
